4061814b268c4d5d33dee45a8d4ccb8bc04d5666
Ansh Goyal <anshgoyal1704@gmail.com>
2023-01-01 15:47:37 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (78.92%) - LeetHub

---
+++ 1227-airplane-seat-assignment-probability/1227-airplane-seat-assignment-probability.java
@@ -0,0 +1,10 @@
+class Solution {
+    public double nthPersonGetsNthSeat(int n) {
+        if(n>1){
+            return 1d/2d;
+        }
+        else{
+            return 1d/1d;
+        }
+    }
+}
