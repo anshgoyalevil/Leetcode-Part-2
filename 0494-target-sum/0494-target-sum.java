d658f8dc42eab44cd73cebdb514f9520ccff4ed5
Ansh Goyal <anshgoyal1704@gmail.com>
2023-07-20 20:05:22 +0530

    Time: 6 ms (75.91%), Space: 43.6 MB (30.51%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,25 +1,52 @@
 class Solution {
-
-    public int f(int t, int e, int[] nums, int id) {
+    
+    public int f(int [] nums, int t, int ind, int [][] dp){
         
-        if (id == nums.length) {
-            if (e == t) {
+        if(ind == 0){
+            if(t == 0 && nums[0] == 0){
+                return 2;
+            }
+            if(t == 0 || nums[0] == t){
                 return 1;
             }
             return 0;
         }
         
-        int a = f(t, e + nums[id], nums, id + 1);
-        int b = f(t, e - nums[id], nums, id + 1);
+        if(dp[ind][t]!=-1){
+            return dp[ind][t];
+        }
         
-        return a+b;
+        int notPick = f(nums, t, ind-1, dp);
+        int pick = 0;
+        if(nums[ind] <= t){
+            pick = f(nums, t-nums[ind], ind-1, dp);
+        }
+        
+        return dp[ind][t] = pick + notPick;
+        
     }
-
-    public int findTargetSumWays(int[] nums, int target) {
+    
+    public int findTargetSumWays(int[] nums, int t) {
         
+        int s = 0;
         
-        return f(target, 0, nums, 0);
+        for(int i = 0; i<nums.length; i++){
+            s+=nums[i];
+        }
         
+        if((s+t)%2==1 || s-t < 0){
+            return 0;
+        }
+        
+        int [][] dp = new int [nums.length][s-t+1];
+        for(int i = 0; i<nums.length; i++){
+            for(int j = 0; j<=s-t; j++){
+                dp[i][j] = -1;
+            }
+        }
+    
+        
+        return f(nums, (s-t)/2, nums.length-1, dp);
         
     }
 }
