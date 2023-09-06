3c7f50d5e3c81eb21a0fc452bad80a876c9b0641
Ansh Goyal <anshgoyal1704@gmail.com>
2023-09-06 22:39:20 +0530

    Time: 6 ms (34.60%), Space: 43.7 MB (39.03%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,39 +1,63 @@
 class Solution {
-    
-    public int f(String a, String b, int i, int j, int [][] dp){
-        
-        if(i<0){
-            return j+1;
-        }
-        if(j<0){
-            return i+1;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
+
+    public int mem(String a, String b) {
+        int n = a.length();
+        int m = b.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 0; i <= m; i++) {
+            dp[0][i] = i;
         }
         
-        if(a.charAt(i) == b.charAt(j)){
-            return f(a, b, i-1, j-1, dp);
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = i;
         }
-        
-        return dp[i][j] = 1 + Math.min(f(a, b, i-1, j, dp), Math.min(f(a, b, i, j-1, dp), f(a, b, i-1, j-1, dp)));
-        
-    }
-    
-    public int minDistance(String word1, String word2) {
-        
-        int n = word1.length();
-        int m = word2.length();
-        
-        int [][] dp = new int [n][m];
-        
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                dp[i][j] = -1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (a.charAt(i - 1) == b.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1]));
+                }
             }
         }
         
-        return f(word1, word2, n-1, m-1, dp);
-        
+        return dp[n][m];
+    }
+
+    public int f(String a, String b, int i, int j, int[][] dp) {
+        if (i < 0) {
+            return j + 1;
+        }
+        if (j < 0) {
+            return i + 1;
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        if (a.charAt(i) == b.charAt(j)) {
+            return f(a, b, i - 1, j - 1, dp);
+        }
+
+        return dp[i][j] = 1 + Math.min(f(a, b, i - 1, j, dp), Math.min(f(a, b, i, j - 1, dp), f(a, b, i - 1, j - 1, dp)));
+    }
+
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+
+        int[][] dp = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                dp[i][j] = -1;
+            }
+        }
+
+        return mem(word1, word2);
+        //return f(word1, word2, n - 1, m - 1, dp);
     }
 }
